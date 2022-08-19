33108386a545ba186dd70ca3f997a28f042c42e1
Sayantan Paul <96608049+Sayantan9999@users.noreply.github.com>
2022-08-19 19:41:09 +0530

    Create assignment_5th.c

---
+++ assignment_5th.c
@@ -0,0 +1,144 @@
+ASSIGNMENT = 5 
+Question = 1
+/*
+#include <stdio.h>
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+printf("my sirg\n");
+}
+*\
+
+Question = 2
+/*
+#include <stdio.h>
+int main()
+{
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+printf("%d\n",i);
+}
+*\
+
+Question =3
+/*
+#include <stdio.h>
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+printf("%d\n",i);
+}
+}
+*\
+
+Question =4
+/*
+#include <stdio.h>
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%d\n",2*i-1);
+}
+}
+*\
+
+Question =5
+/*
+#include <stdio.h>
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+printf("%d\n",2*i-1);
+}
+}
+*\
+
+Question =6
+/*
+#include <stdio.h>
+int main()
+{
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf("%d\n",2*i);
+}
+}
+*\
+
+Question =7
+/*
+#include <stdio.h>
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+printf("%d\n",2*i);
+}
+}
+*\
+
+Question =8
+/*
+#include <stdio.h>
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+printf("%d\n",i*i);
+}
+}
+*\
+
+Question =9
+/*
+#include <stdio.h>
+int main() {
+    // Write C code here
+int i,n;
+printf("enter a number");
+scanf("%d",&n);
+for(i=n;i>=1;i--)
+{
+printf("%d\n",i*i*i);
+}
+}
+*\
+
+Question =10
+/*
+#include <stdio.h>
+int main() {
+  int n, i;
+  printf("Enter an integer: ");
+  scanf("%d", &n);
+  for (i = 1; i <= 10; ++i) {
+    printf("%d * %d = %d \n", n, i, n * i);
+  }
+  return 0;
+}
